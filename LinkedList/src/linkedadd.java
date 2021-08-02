31e6e54c56363df92b1ad178ddcaa4bdd3c1e06a
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-02 19:06:12 +0530

    10th commit
    Upload all files

---
+++ LinkedList/src/linkedadd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class linkedadd {
+
+	public static void main(String[] args) {
+		 LinkedList<String> list = new LinkedList<String>();
+
+	     // Adding elements to the LinkedList
+	     list.add("john");
+	     list.add("priya");
+	     list.add("Tom");
+	     list.add("mammu");
+
+	     // Displaying LinkedList elements
+	     System.out.println("LinkedList elements: "+list);
+	 
+	     // Adding another element
+	     list.add("Kate");
+	     System.out.println("LinkedList elements: "+list);
+	  }
+	}
+	 
+
+	
